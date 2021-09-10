0c1a607f8afae36de231dd36837db753f642b39b
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-09-10 16:42:21 +0300

    Добавит главную страницу

---
+++ src/components/main-page/main-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './main-page.module.scss';
+
+function MainPage() {
+  return (
+    <div className={styles.wrapper}>
+      <header />
+      <main />
+      <footer />
+    </div>
+  );
+}
+
+export default MainPage;
